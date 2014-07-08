d0832031d1f48c57477efdfb2506c220a0f9e41c
Inno Ekeh <innoekeh@gmail.com>
2014-07-08 00:08:10 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
-
-  get "static_pages/about"
-
-  get "static_pages/contacts"
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new', via: 'get'
+  match '/', to: 'static_pages#home', via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   resources :users
 
